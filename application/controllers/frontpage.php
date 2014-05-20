c8cabd16ff788911b35d9b63b7984069ad863288
warrengutierrez <warrengutierrez@5ccdafb8-a043-424b-9403-0fd92412a186>
2014-05-20 12:01:51 +0000

    Locally point and retrieve css and js instead of CDN url googleapi.com

--- application/controllers/frontpage.php
+++ application/controllers/frontpage.php
@@ -1,13 +1,16 @@
-<?php if (!defined('BASEPATH')) die();
-class Frontpage extends Main_Controller {
+<?php
 
-   public function index()
-	{
-      $this->load->view('include/header');
-      $this->load->view('frontpage');
-      $this->load->view('include/footer');
-	}
-   
+if (!defined('BASEPATH'))
+    die();
+
+class Frontpage extends Main_Controller {
+    
+    public function index() {
+        $this->load->view('include/header');
+        $this->load->view('templates/navbar');
+        $this->load->view('include/footer');
+    }
+
 }
 
 /* End of file frontpage.php */
