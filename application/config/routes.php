1b1780d7d2296e3401570a244fa68659ca562199
warrengutierrez <warrengutierrez@5ccdafb8-a043-424b-9403-0fd92412a186>
2014-05-20 08:49:33 +0000

    Adding Twitter Bootstrap and other useful amenities perhaps. ref:https://github.com/sjlu/CodeIgniter-Bootstrap

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,8 +38,9 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "frontpage";
 $route['404_override'] = '';
+
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
