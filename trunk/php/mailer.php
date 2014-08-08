33be2e2f31d57f518c51ea11279cfdbef3641da2
warrengutierrez <warrengutierrez@5ccdafb8-a043-424b-9403-0fd92412a186>
2014-08-08 05:42:37 +0000

    Changed mailer to use STMP instead of internal sendmail via Gmail. Added status loading image on submit.

--- trunk/php/mailer.php
+++ trunk/php/mailer.php
@@ -1,35 +1,34 @@
-﻿<?php 
-
+<?php
 $name = $_POST["name"]; 
 $email = $_POST["email"]; 
 $message = $_POST["message"]; 
 
-
 require_once('PHPMailer_5.2.3/class.phpmailer.php');
 
-$mail             = new PHPMailer();
+$mail = new PHPMailer();
 
-$mail->IsSendmail();
+$mail->IsSMTP();
 
-$body             = $message;
+try {
+  $mail->SMTPAuth   = true;                  // enable SMTP authentication
+  $mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
+  $mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+  $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
+  $mail->Username   = "warren.gutierrez@gmail.com";  // GMAIL username
+  $mail->Password   = "1R0nw1ll2010";            // GMAIL password
+  $mail->AddAddress($mail->Username, 'WG Website');
+  $mail->SetFrom($email, $name);
 
-$mail->SetFrom($email, $name);
-
-$address = "warren.gutierrez@gmail.com"; // YOUR EMAIL ADDRESS
-$nameto = "Warren Gutierrez"; // YOUR NAME
-$mail->AddAddress($address, $nameto); 
-
-$mail->Subject    = "Message from CV contact form";
-
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-
-$mail->MsgHTML($body);
-
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-  echo "Message sent!";
+  $mail->Subject = 'Somebody sent you a message from your website WG.';
+  $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
+  $mail->MsgHTML($message);
+ 
+  $mail->Send();
+  echo "Message Sent - OK";
+} catch (phpmailerException $e) {
+  echo $e->errorMessage(); //Pretty error messages from PHPMailer
+} catch (Exception $e) {
+  echo $e->getMessage(); //Boring error messages from anything else!
 }
-
 
 ?>
